1a8d4f15daa0f64fe42beceae128aecf545fdf12
hyesun03 <chsun0303@gmail.com>
2016-12-12 05:46:40 +0900

    Separate files and add JQuery

---
+++ selected-graph.js
@@ -0,0 +1,15 @@
+var Graph;
+
+Graph = (function() {
+
+    function Graph(target){
+
+    }
+
+})();
+
+$(function() {
+    var selectedGraph, target;
+    target = document.querySelector('.js-calendar-graph');
+    return selectedGraph = new Graph(target);
+});
